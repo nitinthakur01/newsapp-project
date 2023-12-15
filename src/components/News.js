b1ff680f1a6ef3931ca1f6da12d1658f9b472d91
nitinthakur01 <nitinthakur46323@gmail.com>
2023-12-15 16:37:48 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import Newsitem from './Newsitem'
+import PropTypes from 'prop-types'
+
+
+export class News extends Component {
+    
+    static defaultProps = {
+        country: "in",
+        pageSize: 6,
+        category: "general"
+    }
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string,
+    }
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles: [],
+            loading: false,
+            page:1,
+            totalResults: 0
+        }
+        document.title = `${this.props.category} - NewsApp`;
+    }
+    async componentDidMount(){
+        this.props.setProgress(10);
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=07b54890fa264a8fa3a6a44818bacc93&page=1&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+        this.props.setProgress(100);
+    }
+    
+    
+render() {
+    return (
+    <div className='container my-3'>
+        <h1 className='text-center' style={{margin:"30px", marginTop:"90px"}}>NewsApp - top  {this.props.category} headlines</h1>
+        <div className='row'>
+        {this.state.articles.map((element)=>{
+        return  <div className='col-md-4' key={element.url}>
+                <Newsitem  title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
+            </div>
+        })}
+        </div>
+        
+    </div>
+    )
+}
+}
+
+export default News
+
